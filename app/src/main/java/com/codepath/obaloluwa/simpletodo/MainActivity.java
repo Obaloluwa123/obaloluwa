00d506a2307b36946ca409dea25a16a223da2c61
Obaloluwa123 <oolaniran3586@myasu.alasu.edu>
2020-12-17 16:39:09 +0100

    SimpleTodo

--- app/src/main/java/com/codepath/obaloluwa/simpletodo/MainActivity.java
+++ app/src/main/java/com/codepath/obaloluwa/simpletodo/MainActivity.java
@@ -5,12 +5,14 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
@@ -34,8 +36,7 @@ public class MainActivity extends AppCompatActivity {
         etitem = findViewById(R.id.etitem);
         rvitems = findViewById(R.id.rvitems);
 
-
-
+      loadItems();
       items = new ArrayList<>();
       items.add("Buy Milk");
       items.add("Go to the gym");
@@ -49,6 +50,7 @@ public class MainActivity extends AppCompatActivity {
                //Notify the adapter
                itemsAdapter.notifyItemRemoved(position);
                Toast.makeText(getApplicationContext(), "Item was removed", Toast.LENGTH_SHORT).show();
+               saveItems();
 
            }
 
@@ -67,6 +69,7 @@ public class MainActivity extends AppCompatActivity {
                 itemsAdapter.notifyItemInserted(items.size() - 1);
                 etitem.setText("");
                 Toast.makeText(getApplicationContext(), "Item was added", Toast.LENGTH_SHORT).show();
+                saveItems();
             }
         });
     }
@@ -76,7 +79,18 @@ public class MainActivity extends AppCompatActivity {
 
     // This function will load items by reading every line of the data file
     private void loadItems() {
+      try {
         items = new ArrayList<>(FileUtils.readLines(getDataFile(), Charset.defaultCharset());
-    }
+    }   catch (IOException e) {
+          Log.e("MainActivity", "Error reading items", e);
+          items = new ArrayList<>();
+      }
     // This function saves items by writing them into the data file
+        private void saveItems()  {
+          try {
+          FileUtils.writeLines(getDataFile(), items);
+        }   catch (IOException e) {
+              Log.e("MainActivity", "Error writing items", e);
+          }
+        }
 }
